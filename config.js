8468d7880f4bc4440ddd96a57d16d7345b181e55
tzuridis <tzuridis@gmail.com>
2016-09-09 20:49:22 -0400

    travis cli setup heroku

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    (process.env.NODE_ENV === 'production' ?
+    // (process.env.NODE_ENV === 'production' ?
     // 'mongodb://localhost/shopping-list' :
     // 'mongodb://localhost/shopping-list-dev'
     'mongodb://<tzuridis>:<greece1124>@ds029106.mlab.com:29106/mlab-shopping'
