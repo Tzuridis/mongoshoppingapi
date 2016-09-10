45c78a5e03cb32af9422e0f515f0466088b18897
tzuridis <tzuridis@gmail.com>
2016-09-09 20:41:25 -0400

    travis cli setup heroku

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
     // 'mongodb://localhost/shopping-list' :
     // 'mongodb://localhost/shopping-list-dev'
     'mongodb://<tzuridis>:<greece1124>@ds029106.mlab.com:29106/mlab-shopping'
-exports.PORT = process.env.PORT || 8080;
+// exports.PORT = process.env.PORT || 8080;
